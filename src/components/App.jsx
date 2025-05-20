9ca2a0118d32950191eb7f8c465021265f02b554
veronika <artemova331@gmail.com>
2025-05-20 11:49:17 +0300

    create pages and components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import css from './App.module.css';
+import { Router, Routes } from 'react-router-dom';
 
 const App = () => {
-  return <div className={css}>App </div>;
+  return (
+    <div className={css}>
+      <Routes>
+        <Router path="" element />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
