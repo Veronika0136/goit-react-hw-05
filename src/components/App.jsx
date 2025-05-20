0d328b4289ca5eab096817219ece9954ed1e3ae7
veronika <artemova331@gmail.com>
2025-05-20 14:13:14 +0300

    add navlink

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import css from './App.module.css';
-import { Router, Routes } from 'react-router-dom';
+import HomePage from '../pages/HomePage';
+import MoviesPage from '../pages/MoviesPage';
+import MovieDetailsPage from '../pages/MovieDetailsPage';
+import MovieCast from './MovieCast/MovieCast';
+import MovieReviews from './MovieReviews/MovieReviews';
+import NotFoundPage from '../pages/NotFoundPage';
+import Novigation from './Navigation/Navigation';
 
 const App = () => {
   return (
     <div className={css}>
+      <Novigation />
       <Routes>
-        <Router path="" element />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/movies/:movieId" element={<MovieDetailsPage />} />
+        <Route path="/movies/:movieId/cast" element={<MovieCast />} />
+        <Route path="/movies/:movieId/reviews" element={<MovieReviews />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </div>
   );
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import clsx from 'clsx';
+import s from './Navigation.module.css';
+import { NavLink } from 'react-router-dom';
+
+const buildLinkClass = ({ isActive }) => {
+  return clsx(s.link, isActive && s.active);
+};
 
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <header className={s.header}>
+      <nav className={s.nav}>
+        <NavLink to="/" className={buildLinkClass}>
+          Home
+        </NavLink>
+        <NavLink to="/movies" className={buildLinkClass}>
+          Movies
+        </NavLink>
+      </nav>
+    </header>
+  );
 };
 
 export default Navigation;
